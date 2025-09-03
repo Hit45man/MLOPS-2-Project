3dedad89ee6a660b174ceb793f7b60977a9f1237
Rohit Kamble <krohit.kamble@gmail.com>
2025-09-03 17:56:34 +0530

    add web interface

--- api/app.py
+++ api/app.py
@@ -1,10 +1,30 @@
 # api/app.py
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import joblib
 import numpy as np
 
 app = Flask(__name__)
 model = joblib.load("model/registered_model.pkl")
+
+# Species mapping
+species_map = {0: 'Setosa', 1: 'Versicolor', 2: 'Virginica'}
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict_web', methods=['POST'])
+def predict_web():
+    sepal_length = float(request.form['sepal_length'])
+    sepal_width = float(request.form['sepal_width'])
+    petal_length = float(request.form['petal_length'])
+    petal_width = float(request.form['petal_width'])
+    
+    features = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
+    prediction = model.predict(features)[0]
+    species_name = species_map[prediction]
+    
+    return render_template('index.html', prediction=prediction, species_name=species_name)
 
 @app.route('/predict', methods=['POST'])
 def predict():
